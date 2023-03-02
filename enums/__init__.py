23dc0e62dbb47f27a9ff5a32093ccaca356f30b7
Chi Vu <chi.vu@codelink.io>
2023-03-02 19:37:44 +0700

    Add AssetLevel and SearchNonGameAssetType enum

---
+++ enums/__init__.py
@@ -0,0 +1,7 @@
+from .asset_level import AssetLevel
+from .search_non_game_asset_type import SearchNonGameAssetType
+
+__all__ = [
+    "AssetLevel",
+    "SearchNonGameAssetType"
+]
---
+++ enums/asset_level.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class AssetLevel(Enum):
+    FRANCHISE = "franchise"
+    TITLE = "title"
+    BEAT = "beat"
