a25acb605ea072630f57253ecec59ca1196ca42a
Chi Vu <chi.vu@codelink.io>
2023-03-02 19:39:18 +0700

    Add locators for LoginPage

--- ui/login.py
+++ ui/login.py
@@ -5,8 +5,9 @@ from ui.base.base_page import BasePage
 
 class LoginPage(BasePage):
 
-    USERNAME_INPUT = Target.the('The Username Input').located_by('#username')
-    PASSWORD_INPUT = Target.the('The Password Input').located_by('#password')
+    USERNAME_INPUT = Target.the('Username Input').located_by('#username')
+    PASSWORD_INPUT = Target.the('Password Input').located_by('#password')
+    LOG_IN_BUTTON = Target.the('Log In Button').located_by("input[class='login_button']")
 
     def __init__(self, url):
         super().__init__(url)
