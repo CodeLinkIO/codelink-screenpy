43ef94cb2a45f12ddbfcdcc640da9ad9585d64df
Chi Vu <chi.vu@codelink.io>
2023-03-02 20:08:03 +0700

    Add task to set default locales

---
+++ tasks/__init__.py
@@ -0,0 +1,9 @@
+from .login import Login
+from .close_toast_message import CloseToastMessage
+from . import asset_list_game
+
+__all__ = [
+    "Login",
+    "CloseToastMessage",
+    "asset_list_game",
+]
