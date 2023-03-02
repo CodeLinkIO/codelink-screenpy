65b7262b072d298d13daf75b70d0d47af85e9941
Chi Vu <chi.vu@codelink.io>
2023-03-02 19:40:22 +0700

    Add locators for AssetUploadBatchPage

---
+++ ui/__init__.py
@@ -0,0 +1,23 @@
+from .asset_list_game import AssetListGamePage
+from .asset_list_non_game import AssetListNonGamePage
+from .asset_search import AssetSearchPage
+from .asset_upload import AssetUploadPage
+from .asset_upload_batch import AssetUploadBatchPage
+from .channel_ready_library import ChannelReadyLibraryPage
+from .juno_hub import JunoHubPage
+from .login import LoginPage
+from .photo_shoot_library import PhotoShootLibraryPage
+from .youtube_hub import YoutubeHubPage
+
+__all__ = [
+    "AssetListGamePage",
+    "AssetListNonGamePage",
+    "AssetSearchPage",
+    "AssetUploadPage",
+    "AssetUploadBatchPage",
+    "ChannelReadyLibraryPage",
+    "JunoHubPage",
+    "LoginPage",
+    "PhotoShootLibraryPage",
+    "YoutubeHubPage",
+]
