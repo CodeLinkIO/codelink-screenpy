9273845d483145ace9bcbcfcc392adb0c5992280
Chi Vu <chi.vu@codelink.io>
2023-03-02 19:56:26 +0700

    Add task to add beat

---
+++ tasks/asset_list_game/add_beat.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+
+from screenpy import Actor
+from screenpy.pacing import beat
+from screenpy_selenium.actions import Enter, Wait, Click
+from screenpy_selenium import Target
+
+from actions import WaitClick
+from ui import AssetListGamePage
+
+
+class AddBeat:
+
+    DATE_FMT = "%m/%d/%Y"
+
+    def __init__(
+            self,
+            name: str,
+            phase: str,
+            start_date: datetime = None,
+            end_date: datetime = None,
+            is_evergreen: bool = False
+    ):
+        self.name = name
+        self.phase = phase
+        self.start_date = start_date
+        self.end_date = end_date
+        self.is_evergreen = is_evergreen
+
+    def set_date(
+            self,
+            the_actor: Actor,
+            date: datetime,
+            locator: Target
+    ):
+        the_actor.attempts_to(
+            Enter.the_text(date.strftime(self.DATE_FMT)).into_the(locator),
+            Click.on_the(AssetListGamePage.BEAT_MODAL_HEADER)  # Close the date picker
+        )
+
+    @beat("{} adds beat with name '{name}'.")
+    def perform_as(self, the_actor: Actor) -> None:
+        the_actor.attempts_to(
+            WaitClick(AssetListGamePage.ADD_BEAT_BUTTON),
+            Wait.for_the(AssetListGamePage.BEAT_MODAL_HEADER),
+            Enter.the_text(self.name).into_the(AssetListGamePage.BEAT_MODAL_NAME_INPUT),
+            Enter.the_text(self.phase).into_the(AssetListGamePage.BEAT_MODAL_PHASE_DROPDOWN),
+            Click.on_the(AssetListGamePage.BEAT_MODAL_HEADER)
+        )
+        if self.start_date:
+            self.set_date(the_actor, self.start_date, AssetListGamePage.BEAT_MODAL_START_DATE_INPUT)
+        if self.end_date:
+            self.set_date(the_actor, self.end_date, AssetListGamePage.BEAT_MODAL_END_DATE_INPUT)
+        if self.is_evergreen:
+            the_actor.attempts_to(
+                Click.on_the(AssetListGamePage.BEAT_MODAL_IS_EVERGREEN_CHECKBOX)
+            )
+        the_actor.attempts_to(
+            Click.on_the(AssetListGamePage.BEAT_MODAL_CREATE_BUTTON)
+        )
