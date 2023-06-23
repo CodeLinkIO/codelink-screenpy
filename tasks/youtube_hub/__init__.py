00c52683a44f1d9bcfbd99de77c3c0fac0281ab3
Chi Vu <chi.vu@codelink.io>
2023-06-24 00:29:36 +0700

    Add task to search game title

--- tasks/youtube_hub/__init__.py
+++ tasks/youtube_hub/__init__.py
@@ -1,4 +1,9 @@
+from .search_game_title import (SearchGameTitle, SearchGameTitleStatus,
+                                SearchGameTitleType)
 from .search_master_asset import SearchMasterAsset
 __all__ = [
+    "SearchGameTitle",
+    "SearchGameTitleStatus",
+    "SearchGameTitleType",
     "SearchMasterAsset",
 ]
