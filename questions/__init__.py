d9eeff7b951ddbe9527fd2bc2cdda5361762164c
Chi Vu <chi.vu@codelink.io>
2023-06-24 00:41:51 +0700

    Add question to examine the clipboard

--- questions/__init__.py
+++ questions/__init__.py
@@ -1,10 +1,12 @@
 from .api_response import ApiResponse
+from .clipboard_text import ClipboardText
 from .new_tab_url import NewTabUrl
 from .toast_message import ToastMessage
 from . import asset_list_game
 
 __all__ = [
     "ApiResponse",
+    "ClipboardText",
     "NewTabUrl",
     "ToastMessage",
     "asset_list_game"
