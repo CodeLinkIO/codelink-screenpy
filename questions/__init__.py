640af9d04b0faf9a6e0b9581d06a679670d2b30a
Chi Vu <chi.vu@codelink.io>
2023-06-24 00:41:22 +0700

    Add question to examine new tab url

--- questions/__init__.py
+++ questions/__init__.py
@@ -1,9 +1,11 @@
 from .api_response import ApiResponse
+from .new_tab_url import NewTabUrl
 from .toast_message import ToastMessage
 from . import asset_list_game
 
 __all__ = [
     "ApiResponse",
+    "NewTabUrl",
     "ToastMessage",
     "asset_list_game"
 ]
