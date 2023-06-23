cd2785fe02af13ceefa3c3ca8674f0e2cbcc8b74
Chi Vu <chi.vu@codelink.io>
2023-06-24 00:39:58 +0700

    Add question to examine the data on YouTube Landing Dashboard Table

--- questions/youtube_hub/__init__.py
+++ questions/youtube_hub/__init__.py
@@ -1,7 +1,9 @@
 from .master_asset_table_data import MasterAssetTableData
+from .youtube_landing_dashboard_data import YouTubeLandingDashboardData
 from .youtue_metadata_data import YouTubeMetadataData
 
 __all__ = [
     "MasterAssetTableData",
+    "YouTubeLandingDashboardData",
     "YouTubeMetadataData"
 ]
