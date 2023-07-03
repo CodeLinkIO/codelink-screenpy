fa1992f54bbe7040108e125d15827a6c1b3e0b19
chi-codelink <chi.vu@codelink.io>
2023-07-03 17:33:33 +0700

    Setup test level marker

--- enums/__init__.py
+++ enums/__init__.py
@@ -1,5 +1,7 @@
 from .search_non_game_asset_type import SearchNonGameAssetType
+from .test_levels import TestLevels
 
 __all__ = [
-    "SearchNonGameAssetType"
+    "SearchNonGameAssetType",
+    "TestLevels"
 ]
